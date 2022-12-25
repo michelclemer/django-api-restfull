0b74c4ac17c53392a43353195a345ead58aa4a57
Michel Clemer Monteiro Deolino <michelclemermonteirodeolino@gmail.com>
2022-12-25 00:38:47 -0300

    Fix: custom comand django - verify database ready

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -3,15 +3,14 @@ Test custom Django mana
 """
 from unittest.mock import patch
 
-from psycopg2 import OperationalError as Psycopg2Error
-
 from django.core.management import call_command
 from django.db.utils import OperationalError
 from django.test import SimpleTestCase
+from psycopg2 import OperationalError as Psycopg2Error
 
 
 @patch('core.management.commands.wait_for_db.Command.check')
-class CommandTest(SimpleTestCase):
+class CommandTests(SimpleTestCase):
     """Test commands"""
 
     def test_wait_for_db_ready(self, patched_check):
@@ -19,8 +18,7 @@ class CommandTest(SimpleTestCase):
         patched_check.return_value = True
 
         call_command('wait_for_db')
-
-        patched_check.assert_called_once_with(database=['default'])
+        patched_check.assert_called_once_with(databases=['default'])
 
     @patch('time.sleep')
     def test_wait_for_db_delay(self, patched_sleep, patched_check):
@@ -31,4 +29,4 @@ class CommandTest(SimpleTestCase):
         call_command('wait_for_db')
 
         self.assertEqual(patched_check.call_count, 6)
-        patched_check.called_with(database=['default'])
+        patched_check.assert_called_with(databases=['default'])
