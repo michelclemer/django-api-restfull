ad599ab6bfe51f84fc7c7fd76abc5bcecb52366d
Michel Clemer Monteiro Deolino <michelclemermonteirodeolino@gmail.com>
2023-01-31 01:08:48 -0300

    Feat: Create Serializer and views for user

--- app/app/urls.py
+++ app/app/urls.py
@@ -14,12 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/schema/', SpectacularAPIView.as_view(), name='api-schema'),
     path('api/docs/', SpectacularRedocView.as_view(url_name='api-schema'),
-         name='api-docs')
+         name='api-docs'),
+    path('api/user/', include('user.urls'))
 ]
--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -58,7 +58,7 @@ class PublicUserApiTests(TestCase):
             'name': 'Teste name',
         }
 
-        res = self.client(CREATE_USER_URL, payload)
+        res = self.client.post(CREATE_USER_URL, payload)
 
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
         user_exists = get_user_model().objects.filter(
